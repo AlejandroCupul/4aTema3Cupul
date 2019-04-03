facc054d0aacb3dbfb6ace525590d5405c4068d4
Marco Alejandro Cupul Hau <marcoalejandro298@gmail.com>
2019-04-03 15:17:50 -0600

    Interfaz gráfica para calcular la inversa de una matriz.

---
+++ Matriz_Inversa/src/matriz_inversa/Matriz_Inversa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matriz_inversa;
+
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Matriz_Inversa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz Ini = new Interfaz();
+        Ini.setTitle("---CALCULO MATRIZ INVERSA---");
+        Ini.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Ini.setSize(750, 600);
+        Ini.setLocationRelativeTo(null); //centrar
+        Ini.setVisible(true); 
+        //Matriz MAT = new Matriz();
+        //MAT.matr();
+    }
+    
+    
+}
