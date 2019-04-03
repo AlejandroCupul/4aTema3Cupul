b58e67f0e130184d2b5b828542a9c2e55b6b0415
Carlos Daniel Ciau Nahuat <carlosciau10@gmail.com>
2019-04-03 15:39:04 -0600

    Carlos Ciau Aportacion Gausiano

---
+++ Matriz_Inversa/src/matriz_inversa/Gausiano.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matriz_inversa;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Gausiano {
+    
+    public static void Gauss(double a[][], int index[])  {
+            //Metodo Gausiano 
+            int n = index.length;
+            double c[] = new double[n];
+            for (int i=0; i<n; ++i) 
+                index[i] = i;
+            for (int i=0; i<n; ++i) {
+                double c1 = 0;
+                for (int j=0; j<n; ++j) {
+                    double c0 = Math.abs(a[i][j]);
+                    if (c0 > c1) c1 = c0;
+                }
+                c[i] = c1;
+            }
+            int k = 0;
+            for (int j=0; j<n-1; ++j) {
+                double pi1 = 0;
+                for (int i=j; i<n; ++i)  {
+                    double pi0 = Math.abs(a[index[i]][j]);
+                    pi0 /= c[index[i]];
+                    if (pi0 > pi1) {
+                        pi1 = pi0;
+                        k = i;
+                    }
+                }
+                int itmp = index[j];
+                index[j] = index[k];
+                index[k] = itmp;
+                for (int i=j+1; i<n; ++i) {
+                    double pj = a[index[i]][j]/a[index[j]][j];
+
+    
+                    a[index[i]][j] = pj;
+
+                    for (int l=j+1; l<n; ++l)
+                        a[index[i]][l] -= pj*a[index[j]][l];
+                }
+            }
+    }
+    
+}
